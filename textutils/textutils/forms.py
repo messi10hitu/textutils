70ee9c028a5c22e9500db09c22332460aa42ba5e
hitesh <salgotra.hitesh@gmail.com>
2020-02-19 14:32:21 +0530

    there i create the forms

---
+++ textutils/textutils/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+
+class Sign_Up(forms.Form):
+    first_name = forms.CharField(label='first_name', max_length=30)
+    last_name = forms.CharField(label='last_name', max_length=30)
+    emial = forms.EmailField(label='email', max_length=30)
+    password = forms.CharField(label='password', max_length=30)
+
+    def clean(self):
+        cleaned_data = super(Sign_Up, self).clean()
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        email = cleaned_data.get('last_name')
+        password = cleaned_data.get('password')
+
+        if not first_name and not last_name and not email and not password:
+            raise forms.ValidationError('You have to write something!')
+
+
+class Login(forms.Form):
+    email = forms.EmailField(label='email', max_length=30)
+    password = forms.CharField(label='password', max_length=30)
---
+++ textutils/textutils/views.py
@@ -0,0 +1,56 @@
+# i have created this file-hitu
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import Sign_Up, Login
+from .models import Student
+
+
+# render is used to create  and impot the templates
+# render takes first arg = request, 2nd arg = name of the file you want to import, 3rd arg = parameters or variable name
+def index(request):
+    return render(request, 'index.html')
+
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = Sign_Up(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            firstName = form.cleaned_data['first_name']
+            lastName = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            details = Student(first_name=firstName, last_name=lastName, email=email,
+                              password=password)  # these are models variable in red
+            # process the data in form.cleaned_data as required
+            details.save() # this is used to save all the details
+            # ...
+            # redirect to a new URL:
+            return render(request, 'login/new_index.html', {'form': form})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = Sign_Up()
+
+    return render(request, 'login/new_index.html', {'form': form})
+
+
+def login_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = Login(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            return render(request, 'login/new_index.html', {'form': form})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = Login()
+
+    return render(request, 'login/new_index.html', {'form': form})
